6f1754c40f66b8a7c8b6362197802a02a2ef01ee
Pearl Marfatia <pearlm21x@gmail.com>
2023-08-31 16:06:04 +0100

    done task

--- javascript/index.js
+++ javascript/index.js
@@ -3,3 +3,7 @@ console.log(numTest);
 let twoDecimalPoints = numTest.toFixed(2);
 console.log(twoDecimalPoints);
 
+
+let stringTest = `I am the very model of a modern major general`;
+let indexOfM = stringTest.indexOf(`m`);
+console.log(indexOfM);
