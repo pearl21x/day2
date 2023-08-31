0e29aeff1821fa7c76c1f16fdc0ff790f7db4ee5
Pearl Marfatia <pearlm21x@gmail.com>
2023-08-31 16:13:32 +0100

    more changes

--- javascript/index.js
+++ javascript/index.js
@@ -3,5 +3,10 @@ console.log(numTest);
 let twoDecimalPoints = numTest.toFixed(2);
 console.log(twoDecimalPoints);
 let stringTest = `I am the very model of a modern major general`;
-let indexOfM = stringTest.indexOf(`m`);
+stringTest = stringTest.toUpperCase();
+let indexOfM = stringTest.indexOf(`M`);
 console.log(indexOfM);
+let start = stringTest.indexOf("MODEL");
+let end = stringTest.lastIndexOf('MAJOR');
+console.log(start)
+console.log(end)
