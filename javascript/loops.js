56c574059e75443bb807ae68058418a80d163cbb
Pearl Marfatia <pearlm21x@gmail.com>
2023-09-01 13:49:50 +0100

    further loops

--- javascript/loops.js
+++ javascript/loops.js
@@ -1,9 +1,14 @@
-let x = 2
-let i = 0
+// let x = 2
+// let i = 0
 
-while (x < 10000) {
-    x = x ** 2;
-    i++;
-    console.log(x);
-    console.log(i);
-  }
+// while (x < 10000) {
+//     x = x ** 2;
+//     i -= 1;
+//     console.log(x);
+//     console.log(i);
+//   }
+
+// let i = 11
+// for (i; i < 10; i++) {
+//     console.log(i);
+// }
